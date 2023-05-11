c0f5e5585203ce4f1f6bc771195ae885cdc47ede
Andrey Vorobev <mamin.xaker@yandex.ru>
2023-05-12 00:02:31 +0300

    Add tests add cheange package target

---
+++ src/test/java/com/example/LionParameterizedTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class LionParameterizedTest {
+
+    private final String sex;
+    private final boolean expectedHasMane;
+
+    public LionParameterizedTest(String sex, boolean expectedHasMane) {
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[] getData() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
+
+    @Test
+    public void doesHaveManeReturnsCorrectValue() throws Exception {
+        // Arrange
+        Lion lion = new Lion(sex);
+
+        // Act
+        boolean actualHasMane = lion.doesHaveMane();
+
+        // Assert
+        Assert.assertEquals(expectedHasMane, actualHasMane);
+    }
+}
